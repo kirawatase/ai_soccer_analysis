de94ccd6c1a2d50a8e1f3b0b499aa8741beb4b0f
dylanjian0 <dylanjian0@gmail.com>
2024-10-19 16:49:00 -0400

    UI updates: dark theme, video borders

--- foot-analysis-front-end/src/Team.jsx
+++ foot-analysis-front-end/src/Team.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Team = () => {
   return (
-    <div className="font-apple bg-gray-100 min-h-screen flex flex-col items-center m-0">
-      <div className="text-center mt-12">
+    <div className="font-apple bg-gray-800 min-h-screen flex flex-col items-center m-0">
+      <div className="text-center mt-12 text-gray-100">
         <h1 className="text-3xl font-bold">Meet Our Team</h1>
         <p className="mt-4 text-lg">
           Our team consists of three passionate technologists with passion for soccer!
