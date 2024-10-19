6bb7c1df87d5c899600005ea78098a397f8b4ac5
Andy Liu <andy.sixue@gmail.com>
2024-10-19 14:55:58 -0400

    video background implementation

--- foot-analysis-front-end/tailwind.config.js
+++ foot-analysis-front-end/tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+  
   content: [
     './index.html',
     './src/**/*.{js,jsx,ts,tsx}', 
@@ -25,4 +26,3 @@ export default {
   },
   plugins: [],
 }
-
