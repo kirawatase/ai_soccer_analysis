315229181c6e34a9edb2dee23a171d628088c51a
dylanjian0 <dylanjian0@gmail.com>
2024-10-19 09:12:28 -0400

    image and description implementation

--- foot-analysis-front-end/tailwind.config.js
+++ foot-analysis-front-end/tailwind.config.js
@@ -1,8 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    './index.html',
+    './src/**/*.{js,jsx,ts,tsx}', 
+  ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'tech': ['Menlo', 'Monaco', 'Consolas', 'Liberation Mono', 'Courier New', 'monospace'],  // Custom font for "tech-y" look
+      },
+    },
   },
   plugins: [],
 }
