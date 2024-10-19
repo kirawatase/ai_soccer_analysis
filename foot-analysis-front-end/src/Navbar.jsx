57e7acb2c78ffc516c293df813734f8e86c512b6
dylanjian0 <dylanjian0@gmail.com>
2024-10-19 10:22:10 -0400

    link/routing

--- foot-analysis-front-end/src/Navbar.jsx
+++ foot-analysis-front-end/src/Navbar.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';  
 
 const Navbar = () => {
   return (
@@ -6,19 +7,28 @@ const Navbar = () => {
       <div className="flex justify-center">
         <ul className="flex space-x-12 text-gray-100 font-semibold font-tech text-xl">
           <li>
-            <a href="/" className="hover:text-gray-300 border border-gray-100 rounded-lg px-4 py-2 hover:bg-blue-500 transition">
+            <Link 
+              to="/" 
+              className="hover:text-gray-300 border border-gray-100 rounded-md px-4 py-2 hover:bg-blue-500 transition"
+            >
               Home
-            </a>
+            </Link>
           </li>
           <li>
-            <a href="/project" className="hover:text-gray-300 border border-gray-100 rounded-lg px-4 py-2 hover:bg-blue-500 transition">
+            <Link 
+              to="/project" 
+              className="hover:text-gray-300 border border-gray-100 rounded-md px-4 py-2 hover:bg-blue-500 transition"
+            >
               The Project
-            </a>
+            </Link>
           </li>
           <li>
-            <a href="/team" className="hover:text-gray-300 border border-gray-100 rounded-lg px-4 py-2 hover:bg-blue-500 transition">
+            <Link 
+              to="/team" 
+              className="hover:text-gray-300 border border-gray-100 rounded-md px-4 py-2 hover:bg-blue-500 transition"
+            >
               Our Team
-            </a>
+            </Link>
           </li>
         </ul>
       </div>
@@ -27,3 +37,23 @@ const Navbar = () => {
 };
 
 export default Navbar;
+
+//old code
+
+// import React from 'react';
+
+// const Navbar = () => {
+//   return (
+//     <nav className="bg-blue-400 p-4">
+//       <div className="flex justify-center">
+//         <ul className="flex space-x-12 text-gray-100 font-semibold font-tech text-xl">
+//           <li><a href="/" className="hover:text-gray-300 border border-gray-100 rounded-md px-4 py-2 hover:bg-blue-500 transition">Home</a></li>
+//           <li><a href="/project" className="hover:text-gray-300 border border-gray-100 rounded-md px-4 py-2 hover:bg-blue-500 transition">The Project</a></li>
+//           <li><a href="/team" className="hover:text-gray-300 border border-gray-100 rounded-md px-4 py-2 hover:bg-blue-500 transition">Our Team</a></li>
+//         </ul>
+//       </div>
+//     </nav>
+//   );
+// };
+
+// export default Navbar;
